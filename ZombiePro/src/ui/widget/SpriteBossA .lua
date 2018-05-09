51e5203485d0cb1739dc1a1921d05f88247b79c5
Heylen <383680491@qq.com>
2018-05-09 23:40:19 +0800

    ZombiePro/src/ui/widget/SpriteBossA .lua

--- ZombiePro/src/ui/widget/SpriteBossA .lua
+++ ZombiePro/src/ui/widget/SpriteBossA .lua
@@ -1,10 +1,10 @@
-local SpriteBossA = class('SpriteBossA', require 'ui.base.SpriteMonster')
-local SkillCDTime = 4
-local SkillBefore = 2
-local SkillAttackRadius = 150
+local SpriteBossA = class('SpriteBossA', require 'ui.widget.SpriteMonster')
+local SkillCDTime = 5
+local SkillBefore = 0.6
+local SkillAttackRadius = 500
 
 function SpriteBossA:ctor(...)
-    SpriteBoss1.super.ctor(self, ...)
+    SpriteBossA.super.ctor(self, ...)
     self.skillCDTime = SkillCDTime
     self.skillAttackRadius = SkillAttackRadius
     self.mainSprite:loadTexture('monster_4_1.png')
@@ -28,14 +28,19 @@ function SpriteBossA:skill()
     local warnCircle = ccui.ImageView:create('battle/button_shoot.png', 1)
     warnCircle:setPosition(cc.p(tx, ty))
     self.gameLayer.mainMap:addChild(warnCircle)
-    warnCircle:runAction(cc.Sequence:create(cc.Bink:create(SkillBefore, 6), cc.CallFunc:create(function(pender) 
+    warnCircle:runAction(cc.Sequence:create(cc.Blink:create(SkillBefore, 5), cc.CallFunc:create(function(pender) 
         pender:removeFromParent()
     end)))
 
+    local dir = G_Utils.getDirVector(cc.p(x, y), cc.p(tx, ty))
+    local radian = cc.pToAngleSelf(dir)
+    local angle = radian / 3.14159 * 180 --弧度变角度 
+    self.mainNode:setRotation(270 - angle)   --重新确定方向
+
     self:runAction(cc.Sequence:create(
-        cc.DelayTime:create(SkillBefore)
-        cc.Spawn:create(cc.ScaleTo(0.5, 2), cc.MoveTo:create(0.5, cc.pMidpoint(cc.p(x, y), cc.p(tx, ty)))),
-        cc.Spawn:create(cc.ScaleTo(0.5, 1), cc.MoveTo:create(0.5, cc.p(tx, ty))),
+        cc.DelayTime:create(SkillBefore),
+        cc.Spawn:create(cc.ScaleTo:create(0.5, 3), cc.MoveTo:create(0.5, cc.pMidpoint(cc.p(x, y), cc.p(tx, ty)))),
+        cc.Spawn:create(cc.ScaleTo:create(0.5, 1), cc.MoveTo:create(0.5, cc.p(tx, ty))),
         cc.CallFunc:create(function() 
             self.isForceAction = false
             self.status = 'stand'
